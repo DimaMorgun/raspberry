2ea1be0334128458d9a4a6ce0e26a462e1dd0600
DimaMorgun <dimamorgun97@gmail.com>
2022-04-25 12:10:34 +0300

    completed v1 web example site.

--- web-example/site.js
+++ web-example/site.js
@@ -1,15 +1,34 @@
-const http = require("http");
-
-const host = '192.168.0.138';
+const express = require('express');
+const app = express();
 const port = 5000;
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(`<html><body><h1>Good evening, we are from Ukraine!</h1></body></html>`);
-};
+const gpio = require('onoff').Gpio;
+const led = new gpio(4, 'out');
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+app.get('/', (req, res) => {
+    res.send('Good evening, we are from Ukraine!');
 });
+
+app.get('/light/:enable', (req, res) => {
+    setLed(req.params.enable);
+
+    res.send({ success: true });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+function setLed(enable) {
+    if (enable === '1') {
+        console.log(`Turn on with ${enable}`);
+
+        led.writeSync(1);
+    } else if (enable === '0') {
+        console.log(`Turn off with ${enable}`);
+
+        led.writeSync(0);
+    } else {
+        console.log(`Nothing with ${enable}`);
+    }
+}
